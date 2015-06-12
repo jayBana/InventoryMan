df50fb6cbbdb8d486c958ade1463654c9fa6711d
Janos Bana <janosbana@gmail.com>
2015-06-12 14:22:37 +0100

    Changes to configuration files

--- G53IDS/_config.py
+++ G53IDS/_config.py
@@ -8,7 +8,7 @@ PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'uni_of_nottingham'
 THREADED = False
-DEBUG = True
+DEBUG = False
 
-# port needs to be defined if it want it to run in vagrant
+# port needs to be defined if we want to run in Vagrant
 PORT = 8080
